600652b94699bd37bb0e8b7f978cd3761865a43b
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2023-10-29 21:49:01 +0700

    API customer

--- server(new)/controllers/customerController.js
+++ server(new)/controllers/customerController.js
@@ -9,6 +9,70 @@ const customerController = {
       });
     } catch (error) {
       console.log(error);
+      res.json({
+        state: "error",
+      });
+    }
+  },
+  getById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const [rows, fields] = await connection.promise().query("select * from customers where id = ?", [id]);
+      res.json({
+        data: rows,
+      });
+    } catch (error) {
+      console.log(error);
+      res.json({
+        state: "error",
+      });
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const { name, email, phone, password } = req.body;
+      // console.log("line 34 customer controller", req.body);
+      const sql = "INSERT INTO customers (name, email, phone, password) VALUES (?, ?, ?, ?)";
+      const [rows, fields] = await connection.promise().query(sql, [name, email, phone, password]);
+      // console.log("line 36 customer controller", fields);
+      res.json({
+        data: rows,
+      });
+    } catch (error) {
+      console.log(error);
+      res.json({
+        state: "error",
+      });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const { name, email, phone, password } = req.body;
+      const { id } = req.params;
+      const sql = "update customers set name = ?,email = ?,phone = ?,password = ? where id = ?";
+      const [rows, fields] = await connection.promise().query(sql, [name, email, phone, password, id]);
+      res.json({
+        data: rows,
+      });
+    } catch (error) {
+      console.log(error);
+      res.json({
+        state: "error",
+      });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const [rows, fields] = await connection.promise().query("delete from customers where id = ?", [id]);
+      res.json({
+        data: rows,
+      });
+    } catch (error) {
+      console.log(error);
+      res.json({
+        state: "error",
+      });
     }
   },
 };
--- server(new)/routers/customerRouter.js
+++ server(new)/routers/customerRouter.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const customerController = require("../controllers/customerController");
 
 router.get("/", customerController.getALL);
+router.get("/:id", customerController.getById);
+router.post("/", customerController.create);
+router.put("/:id", customerController.update);
+router.delete("/:id", customerController.delete);
 
 module.exports = router;
