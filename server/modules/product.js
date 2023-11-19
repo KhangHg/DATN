d8133c233c1bfb5faa5e63f4b097b990c58ecf13
KhangHg <nghia.hnkt@gmail.com>
2023-11-19 14:53:04 +0700

    update product CRUD api

--- server/modules/product.js
+++ server/modules/product.js
@@ -3,7 +3,7 @@ const connection = require("../database/connectDB");
 connection.connect((err) => {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE IF NOT EXISTS products (productId INT AUTO_INCREMENT PRIMARY KEY,description VARCHAR(255), price DECIMAL(10,2), categoryId INT, sizeId INT, imageUrl VARCHAR(255),  FOREIGN KEY (categoryId) REFERENCES categories(categoryId),  FOREIGN KEY (sizeId) REFERENCES size(sizeId)  )";
+    var sql = "CREATE TABLE IF NOT EXISTS products (productId INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255), description VARCHAR(255), price DECIMAL(10,2), categoryId INT, sizeId INT,quantity INT,  imageUrl VARCHAR(255),  FOREIGN KEY (categoryId) REFERENCES categories(categoryId),  FOREIGN KEY (sizeId) REFERENCES size(sizeId))";
     connection.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Table products created");
--- server/routers/productRouter.js
+++ server/routers/productRouter.js
@@ -5,5 +5,8 @@ const productController = require("../controllers/productController")
 
 router.get("/", productController.getAll)
 router.get("/:product_id", productController.getById)
+router.post("/", productController.create)
+router.put("/:id", productController.update)
+router.delete("/", productController.delete)
 
 module.exports = router
