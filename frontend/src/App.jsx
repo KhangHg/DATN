c69ec8fb3bd8800099020b5289361d6de298ce49
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2023-12-10 21:25:26 +0700

    aaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { userRoutes, adminRoutes } from "../src/routes";
 import DefaultLayoutAdmin from "./layouts/AdminLayout/DefaultLayout/DefaultLayout";
 import DefaultLayoutUser from "./layouts/UserLayout/DefaultLayOutUser";
@@ -12,9 +12,7 @@ import { ToastContainer } from "react-toastify";
 
 function App() {
   const { token, user } = useContext(AuthContext);
-  const navigateTo = useNavigate();
-  const tokenLocal = localStorage.getItem("token");
-  const role = localStorage.getItem("role");
+
   return (
     <div className="App">
       <Routes>
@@ -44,7 +42,7 @@ function App() {
         {/* privateRoutes */}
         {/* {user && user.role === "ADMIN" ? // Kiểm tra có phải là admin hay không */}
         {/* Cứ làm các trang của admin đi, khi nào xong hết thì thêm xác thực user sau */}
-        {/* {
+        {
           adminRoutes.map((route, index) => {
             const Page = route.component;
             let Layout = DefaultLayoutAdmin;
@@ -59,41 +57,13 @@ function App() {
                 element={
                   <Layout title={route.title}>
                     <Page />
-                    <ToastContainer position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="colored" />
                   </Layout>
                 }
               />
             );
           })
           // :''}
-        } */}
-        {adminRoutes.map((route, index) => {
-          // Check if there is a token and the role is "admin"
-          if (!tokenLocal || !role || role !== "admin") {
-            // Redirect to login if the conditions are not met
-            navigateTo("/login");
-            return null;
-          }
-
-          const Page = route.component;
-          let Layout = DefaultLayoutAdmin;
-          if (route.layout) {
-            Layout = route.layout;
-          }
-
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout title={route.title}>
-                  <Page />
-                  <ToastContainer position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="colored" />
-                </Layout>
-              }
-            />
-          );
-        })}
+        }
 
         {/* Điều hướng mặc định */}
         <Route path="*" element={<Navigate to="/" />} />
