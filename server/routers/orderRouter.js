fc4eb44f46d7bc6d3248de30dad3a59e7b0ef231
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2023-12-10 21:22:41 +0700

    Hello Nghia

--- server/routers/orderRouter.js
+++ server/routers/orderRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 
 router.get("/", authUser, authRoleAdmin, orderController.getALL);
-router.post("/", authUser, authRoleUser, orderController.createOrder);
+router.post("/", orderController.createOrder);
 router.put("/:id", orderController.updateStatus);
 router.get("/:id", orderController.getDetailOrder);
 // router.delete("/:id", productController.delete)
