ff5d50d391c07773786ebb6c17c8c1f0f40b61cb
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2023-12-12 10:50:40 +0700

    friend

--- frontend/src/services/getOneProduct.jsx
+++ frontend/src/services/getOneProduct.jsx
@@ -21,13 +21,3 @@ export const getOneProduct = async (id) => {
     console.log("getOneProduct " + error);
   }
 };
-export const getOneProduct1 = async (id) => {
-  try {
-    const res = await request.get(`/product/${id}`, {
-      params: {},
-    });
-    return res.data;
-  } catch (error) {
-    console.log("getOneProduct " + error);
-  }
-};
